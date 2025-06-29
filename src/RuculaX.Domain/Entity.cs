95eced51bf82ad6dc6e0c9b4d956252d86b15a4f
reginaldo-marinho <you@example.com>
2025-06-29 00:13:06 -0300

    Adicionando verificação  para a identidade unica das   entidades

--- src/RuculaX.Domain/Entity.cs
+++ src/RuculaX.Domain/Entity.cs
@@ -6,8 +6,25 @@
 /// <typeparam name="T"></typeparam>
 public class Entity<T>
 {
-    public T? Id {get;set;}   
+    public Entity() { }
+    public Entity(T id)
+    {
+        this.Id = id;
+    }
+
+    public T? Id { get; private set; }
+    public void SetId(T id) {
+        CheckId(id);
+        Id = id;
+    }
     
+    public void CheckId(T id)
+    {
+        if (id is not null)
+        {
+            throw new RuculaxEntityBaseException(RuculaxEntityBaseException.AlreadyHasAnIdentity);
+        }
+    }
 }
 /// <summary>
 /// For Entities that have more than one field in the composition of their unique identity
